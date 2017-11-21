3026d632ee6b53128adc3cb19cb3ec94ef6d4a1f
XueZhang <xue.zhang@aayongche.com>
2017-11-21 10:20:13 +0800

    text

--- app/src/main/java/com/nuts/sample/activity/MainActivity.java
+++ app/src/main/java/com/nuts/sample/activity/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //zhangxue test
     }
 
     public void onClick(View view) {
