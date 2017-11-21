6ace6ca2f8e9cda1c5612431dfe172d8f027454d
XueZhang <xue.zhang@aayongche.com>
2017-11-21 14:34:43 +0800

    umeng配置

--- app/src/main/java/com/nuts/sample/GlobalApplication.java
+++ app/src/main/java/com/nuts/sample/GlobalApplication.java
@@ -3,6 +3,7 @@ package com.nuts.sample;
 import com.google.gson.Gson;
 import com.nuts.lib.BaseApplication;
 import com.nuts.sample.config.Const;
+import com.umeng.commonsdk.UMConfigure;
 
 // TODO: zhangxue，youmeng 打点统计
 public class GlobalApplication extends BaseApplication {
@@ -15,5 +16,22 @@ public class GlobalApplication extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //设置LOG开关，默认为false
+        UMConfigure.setLogEnabled(true);
+        /**
+         * 设置日志加密
+         * 参数：boolean 默认为false（不加密）
+         */
+        UMConfigure.setEncryptEnabled(true);
+        /**
+         * 初始化common库
+         * 参数1:上下文，不能为空
+         * 参数2:设备类型，UMConfigure.DEVICE_TYPE_PHONE为手机、UMConfigure.DEVICE_TYPE_BOX为盒子，默认为手机
+         * 参数3:Push推送业务的secret
+         *如果您的应用使用了混淆， 请添加 -keep class com.umeng.commonsdk.** {*;}
+         */
+        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, "");
+
     }
 }
--- app/src/main/java/com/nuts/sample/activity/BaseActivity.java
+++ app/src/main/java/com/nuts/sample/activity/BaseActivity.java
@@ -1,6 +1,38 @@
 package com.nuts.sample.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.analytics.MobclickAgent.EScenarioType;
 
 public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /**
+         * 场景类型设置接口:
+         * etype是官方场景，有如下场景:
+
+         EScenarioType.E_UM_NORMAL 普通统计场景类型
+
+         EScenarioType.E_UM_GAME 游戏场景类型
+         */
+
+        MobclickAgent.setScenarioType(this, EScenarioType.E_UM_NORMAL);
+        MobclickAgent.setSessionContinueMillis(1000);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
 }
